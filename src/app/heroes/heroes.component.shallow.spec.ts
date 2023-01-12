15cad9ee1f56abd16485f0753431348301d14e21
Alan Nunez <Marcelina28@github.com>
2023-01-12 14:45:26 -0400

    Getting a hold of the DOM

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,6 +1,7 @@
 import { HeroComponent } from './../hero/hero.component';
 import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { HeroService } from '../hero.service';
+import { By } from '@angular/platform-browser';
 
 describe('Shallow test', () => {
   let fixture: ComponentFixture<HeroComponent>;
@@ -18,4 +19,11 @@ describe('Shallow test', () => {
      fixture.componentInstance.hero = { id: 1, name: "SuperDude", strength: 3};
      expect(fixture.componentInstance.hero.name).toBe('SuperDude');
   })
+
+  it('Vanilla DOM test', () => {
+    fixture.componentInstance.hero = { id: 1, name: "SuperDude", strength: 3};
+    fixture.detectChanges();
+    let debugElement = fixture.debugElement.query(By.css('a'));
+    expect(debugElement.nativeElement.textContent).toContain('SuperDude');
+ })
 })
