b6985ba0b1a5347363d3f3fe61e4ed1cd55bd437
Alan Nunez <Marcelina28@github.com>
2023-01-13 19:40:14 -0400

    Testing input values

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,9 +1,14 @@
-import { TestBed } from "@angular/core/testing";
+import { HttpClientModule } from '@angular/common/http';
+import { HeroService } from './../hero.service';
+import { HeroComponent } from './../hero/hero.component';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { of } from "rxjs";
 import { HeroesComponent } from "./heroes.component";
+import { By } from '@angular/platform-browser';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe("HeroesComponent", () => {
-  let component: HeroesComponent;
+  let component: ComponentFixture<HeroesComponent>;
   let HEROES;
   let mockHeroService;
 
@@ -19,28 +24,70 @@ describe("HeroesComponent", () => {
       "addHero",
       "deleteHero",
     ]);
-    component = new HeroesComponent(mockHeroService);
+    TestBed.configureTestingModule({
+      declarations:[HeroesComponent, HeroComponent],
+      imports:[HttpClientModule],
+      providers: [{ provide: HeroService, useValue: mockHeroService
+     }],
+     schemas: [NO_ERRORS_SCHEMA]
+    })
+    component = TestBed.createComponent(HeroesComponent);
   });
 
   describe("Delete", () => {
     it("should delete one of the elements", () => {
       mockHeroService.deleteHero.and.returnValue(of(true));
-      component.heroes = HEROES;
-      component.delete(HEROES[2]);
-      expect(component.heroes.length).toBe(2);
+      component.componentInstance.heroes = HEROES;
+      component.componentInstance.delete(HEROES[2]);
+      expect(component.componentInstance.heroes.length).toBe(2);
     });
 
     it("should call deleteHero", () => {
       mockHeroService.deleteHero.and.returnValue(of(true));
-      component.heroes = HEROES;
-      component.delete(HEROES[2]);
+      component.componentInstance.heroes = HEROES;
+      component.componentInstance.delete(HEROES[2]);
       expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
     });
   });
 
   it("set the heroes correctly", () => {
-    // mockHeroService.getHeroes.and.returnValue(of(HEROES));
-    // let fixture = TestBed.createComponent(HeroesComponent);
-    // expect(fixture.componentInstance.heroes.length).toBe(3);
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    component.detectChanges();
+    expect(component.componentInstance.heroes.length).toBe(3);
   });
-});
+
+  it('should call the heroService.deleteHero when the Hero Component delete button is clicked', () => {
+    spyOn(component.componentInstance, 'delete');
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    component.detectChanges();
+
+    const heroComponents = component.debugElement.queryAll(By.directive(HeroComponent));
+    (<HeroComponent>heroComponents[0].componentInstance).delete.emit();
+
+    expect(component.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+  })
+
+  it('should add a new  hero to the hero list when the add button is clicked', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    component.detectChanges();
+
+    mockHeroService.addHero.and.returnValue(of({
+      id: 1,
+      name: "Mr Jackson",
+      strength: 4
+    }));
+
+    let input = component.debugElement.query(By.css('input')).nativeElement;
+    input.value = "Mr Jackson";
+
+    let button = component.debugElement.queryAll(By.css('button'))[0];
+    button.triggerEventHandler('click', null);
+
+    component.detectChanges();
+
+    const heroText = component.debugElement.query(By.css('ul')).nativeElement.textContent;
+    expect(heroText).toContain("Mr Jackson");
+    console.log("heroText", heroText)
+   });
+})
